5f52de5f72434673ba3013858b2d215b1e5d30cd
HsuBokai <exp2.718281@gmail.com>
2019-01-16 15:45:10 +0000

    gcc/ld use 32 bits mode

--- run.sh
+++ run.sh
@@ -10,11 +10,14 @@ dd if=/tmp/mbr.bin of=hd60M.img  bs=512 conv=notrunc
 nasm -I ./src/include/ -o /tmp/loader.bin ./src/loader.S
 dd if=/tmp/loader.bin of=hd60M.img  bs=512 seek=2 conv=notrunc
 
-gcc -c -o /tmp/main.o ./src/kernel/main.c
-ld /tmp/main.o -Ttext 0xc0001500 -e main -o /tmp/kernel.bin
+gcc -m32 -c -o /tmp/main.o ./src/kernel/main.c
+ld -m elf_i386 /tmp/main.o -Ttext 0xc0001500 -e main -o /tmp/kernel.bin
 
-xxd -u -a -g 1 -s 0 -l 0x40 /tmp/kernel.bin
-xxd -u -a -g 1 -s 0x40 -l 0x38 /tmp/kernel.bin
+elf_header_size=`readelf -e /tmp/kernel.bin | grep "Size of this header" | cut -d ":" -f2 | cut -d "(" -f1`
+xxd -u -a -g 1 -s 0 -l $elf_header_size /tmp/kernel.bin
+program_header_start=`readelf -e /tmp/kernel.bin | grep "Start of program header" | cut -d ":" -f2 | cut -d "(" -f1`
+program_header_size=`readelf -e /tmp/kernel.bin | grep "Size of program header" | cut -d ":" -f2 | cut -d "(" -f1`
+xxd -u -a -g 1 -s $program_header_start -l $program_header_size /tmp/kernel.bin
 
 kernel_size=`ls -l /tmp/kernel.bin | cut -d ' ' -f5`
 if [ $kernel_size -gt 51200 ]; then

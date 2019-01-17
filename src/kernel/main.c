8b4c2e686b9fffe70863b20a121de0d8cc1f310b
HsuBokai <exp2.718281@gmail.com>
2019-01-17 14:07:18 +0000

    test basic asm code in .c file

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,13 @@
+int in_a=1, in_b=2, out_sum;
 int main(void){
+	asm(" \
+		pusha;			\
+		movl in_a, %eax;	\
+		movl in_b, %ebx;	\
+		addl %ebx, %eax;	\
+		movl %eax, out_sum;	\
+		popa			\
+	");
 	while(1);
 	return 0;
 }

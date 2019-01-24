e480fe8d757d15f3a75e694dee37a275e600b344
HsuBokai <exp2.718281@gmail.com>
2019-01-24 15:38:37 +0000

    use gs:video_ptr to put_str

--- src/kernel/print.c
+++ src/kernel/print.c
@@ -1,23 +1,28 @@
 #include "stdint.h"
 
-#define VIDEO_DESC_BASE_HIGH_H	0x00
-#define VIDEO_DESC_BASE_HIGH_L	0x0b
-#define VIDEO_DESC_BASE_LOW	0x8000
-#define VIDEO_DESC_BASE		((VIDEO_DESC_BASE_HIGH_H << 24) | (VIDEO_DESC_BASE_HIGH_L << 16) | VIDEO_DESC_BASE_LOW)
+uint32_t video_ptr = 0;
+uint16_t video_char = 0x0700;
 
 void put_str(uint8_t* str)
 {
-	static uint16_t* video_ptr = (uint16_t*)VIDEO_DESC_BASE;
 
 	if (0 == str)
 		return;
 
 	for(str; '\0' != *str; str++) {
 		if ('\n' == *str) {
-			video_ptr -= (video_ptr - (uint16_t*)VIDEO_DESC_BASE) % 80;
+			video_ptr -= (video_ptr % 80);
 			video_ptr += 80;
 		} else {
-			(*video_ptr) = (uint16_t)(0x0700 | *str);
+			video_char = (uint16_t)(0x0700 | *str);
+			asm volatile(" \
+				pusha;				\
+				movl video_ptr, %eax;		\
+				shll $1, %eax;			\
+				movw video_char, %ebx;		\
+				movw %ebx, %gs:(%eax);		\
+				popa;				\
+			");
 			video_ptr++;
 		}
 	}

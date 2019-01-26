71f5c38f14198ee2c5234cedc9be52ca637435be
HsuBokai <exp2.718281@gmail.com>
2019-01-26 10:22:24 +0000

    readelf and objdump in run.sh

--- run.sh
+++ run.sh
@@ -17,17 +17,21 @@ gcc -m32 -c -o /tmp/print.o ./src/kernel/print.c -I ./src/kernel/include
 gcc -m32 -c -o /tmp/main.o ./src/kernel/main.c -I ./src/kernel/include
 ld -m elf_i386 /tmp/main.o /tmp/print.o /tmp/interrupt.o -Ttext 0xc0010000 -e main -o /tmp/kernel.bin
 
-elf_header_size=`readelf -e /tmp/kernel.bin | grep "Size of this header" | cut -d ":" -f2 | cut -d "(" -f1`
-xxd -u -a -g 1 -s 0 -l $elf_header_size /tmp/kernel.bin
-program_header_start=`readelf -e /tmp/kernel.bin | grep "Start of program header" | cut -d ":" -f2 | cut -d "(" -f1`
-program_header_size=`readelf -e /tmp/kernel.bin | grep "Size of program header" | cut -d ":" -f2 | cut -d "(" -f1`
-xxd -u -a -g 1 -s $program_header_start -l $program_header_size /tmp/kernel.bin
+#elf_header_size=`readelf -e /tmp/kernel.bin | grep "Size of this header" | cut -d ":" -f2 | cut -d "(" -f1`
+#xxd -u -a -g 1 -s 0 -l $elf_header_size /tmp/kernel.bin
+#program_header_start=`readelf -e /tmp/kernel.bin | grep "Start of program header" | cut -d ":" -f2 | cut -d "(" -f1`
+#program_header_size=`readelf -e /tmp/kernel.bin | grep "Size of program header" | cut -d ":" -f2 | cut -d "(" -f1`
+#xxd -u -a -g 1 -s $program_header_start -l $program_header_size /tmp/kernel.bin
 
 kernel_size=`ls -l /tmp/kernel.bin | cut -d ' ' -f5`
 if [ $kernel_size -gt 51200 ]; then
 	echo "kernel image too large!!!"
 	exit 1
 fi
+
+readelf -e /tmp/kernel.bin
+
+objdump -t /tmp/kernel.bin
 
 dd if=/tmp/kernel.bin of=hd60M.img bs=512 seek=9 conv=notrunc
 [ $? -ne 0 ] && echo "compile kernel.bin fail!" && exit 1

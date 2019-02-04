4d84636fd5591f799d3bb3b85b66394ad542c20f
HsuBokai <exp2.718281@gmail.com>
2019-02-04 20:59:45 +0800

    test malloc_page

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,7 @@
 #include "debug.h"
 #include "interrupt.h"
 #include "print.h"
+#include "mm.h"
 
 int in_a=1, in_b=2, out_sum;
 
@@ -15,6 +16,12 @@ int main(void){
 	");
 	put_str("hello world!\n");
 	put_str("This is kernel!\n");
+
+	mem_init();
+	//malloc_page(PF_KERNEL, 10);
+	//malloc_page(PF_KERNEL, 20);
+	//malloc_page(PF_KERNEL, 30);
+	//malloc_page(PF_KERNEL, 40);
 
 	idt_init();
 	asm volatile("sti");

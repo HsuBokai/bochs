e0f3f8dedf8cd63c119dbf19e72fdfdf26640c49
HsuBokai <exp2.718281@gmail.com>
2019-02-05 15:11:43 +0800

    add interrupt name msg

--- src/kernel/interrupt.c
+++ src/kernel/interrupt.c
@@ -2,6 +2,7 @@
 #include "global.h"
 #include "io.h"
 #include "print.h"
+#include "debug.h"
 
 #define IDT_DESC_CNT 0x21
 
@@ -20,6 +21,7 @@ struct gate_desc {
 
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
 static struct gate_desc idt[IDT_DESC_CNT];
+static int8_t* intr_name[IDT_DESC_CNT];
 
 extern intr_handler intr_entry_table[IDT_DESC_CNT];
 
@@ -27,7 +29,10 @@ void general_intr_handler(uint8_t vec_nr)
 {
 	put_str("interrupt!! vec_nr:");
 	put_int(vec_nr);
-	put_str("!\n");
+	put_str("!! intr_name:");
+	put_str(intr_name[vec_nr]);
+	put_str("\n");
+	ASSERT(0);
 }
 
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function) {
@@ -43,6 +48,42 @@ static void idt_desc_init(void) {
 	for (i=0; i<IDT_DESC_CNT; i++) {
 		make_idt_desc(&idt[i], IDT_DESC_ATTR_DPL0, intr_entry_table[i]);
 	}
+	intr_name[0x00] = "#DE Divide Error";
+	intr_name[0x01] = "#DB Debug Exception";
+	intr_name[0x02] = "#NMI Interrupt";
+	intr_name[0x03] = "#BP Breakpoint Exception";
+	intr_name[0x04] = "#OF Overflow Exception";
+	intr_name[0x05] = "#BR Bound Range Exceeded Exception";
+	intr_name[0x06] = "#UD Invalid Opcode Exception";
+	intr_name[0x07] = "#NM Device Not Available Exception";
+	intr_name[0x08] = "#DF Double Fault Exception";
+	intr_name[0x09] = "#CSO Coprocessor Segment Overrun";
+	intr_name[0x0a] = "#TS Invalid TSS Exception";
+	intr_name[0x0b] = "#NP Segment Not Present";
+	intr_name[0x0c] = "#SS Stack Fault Exception";
+	intr_name[0x0d] = "#GP General Protection Exception";
+	intr_name[0x0e] = "#PF Page-Fault Exception";
+	intr_name[0x0f] = "#UU Unused";
+
+	intr_name[0x10] = "#MF x87 FPU Floating-Point Error";
+	intr_name[0x11] = "#AC Alignment Check Exception";
+	intr_name[0x12] = "#MC Machine-Check Exception";
+	intr_name[0x13] = "#XF SIMD Floating-Point Exception";
+	intr_name[0x14] = "#UK Unknown";
+	intr_name[0x15] = "#UK Unknown";
+	intr_name[0x16] = "#UK Unknown";
+	intr_name[0x17] = "#UK Unknown";
+	intr_name[0x18] = "#UK Unknown";
+	intr_name[0x19] = "#UK Unknown";
+	intr_name[0x1a] = "#UK Unknown";
+	intr_name[0x1b] = "#UK Unknown";
+	intr_name[0x1c] = "#UK Unknown";
+	intr_name[0x1d] = "#UK Unknown";
+	intr_name[0x1e] = "#UK Unknown";
+	intr_name[0x1f] = "#UK Unknown";
+
+	intr_name[0x20] = "#UK Unknown";
+
 	put_str("idt_desc_init done\n");
 }
 

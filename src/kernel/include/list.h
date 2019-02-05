a171b28453b7e5e5b943126458fbfe0891147af4
HsuBokai <exp2.718281@gmail.com>
2019-02-05 17:25:03 +0800

    fix double link list bugs

--- src/kernel/include/list.h
+++ src/kernel/include/list.h
@@ -2,14 +2,24 @@
 #define __LIB_LIST_H
 #include "stdint.h"
 
+typedef struct list_elem {
+	struct list_elem* prev;
+	struct list_elem* next;
+} list_elem_t;
+
+
+void list_elem_init(list_elem_t *self);
+void list_elem_insert_before(list_elem_t *self, list_elem_t *new_elem);
+void list_elem_insert_after(list_elem_t *self, list_elem_t *new_elem);
+
 typedef struct list {
-	struct list* prev;
-	struct list* next;
+	list_elem_t *head;
+	list_elem_t *tail;
 } list_t;
 
 void list_init(list_t *self);
-void list_insert_before(list_t *elem, list_t *new_elem);
-void list_insert_after(list_t *elem, list_t *new_elem);
-int elem_find(list_t *self, list_t *elem);
+void list_insert_head(list_t *self, list_elem_t *elem);
+void list_insert_tail(list_t *self, list_elem_t *elem);
+int list_find_elem(list_t *self, list_elem_t *elem);
 
 #endif /* __LIB_LIST_H */
--- src/kernel/list.c
+++ src/kernel/list.c
@@ -1,43 +1,78 @@
 #include "list.h"
 #include "debug.h"
 
-void list_init(list_t *self)
+void list_elem_init(list_elem_t *self)
 {
 	self->next = NULL;
 	self->prev = NULL;
 }
 
-void list_insert_before(list_t *elem, list_t *new_elem)
+void list_elem_insert_before(list_elem_t *self, list_elem_t *new_elem)
 {
-	ASSERT(NULL != elem);
+	ASSERT(NULL != self);
 	ASSERT(NULL != new_elem);
 
-	new_elem->prev = elem->prev;
-	new_elem->next = elem;
-	elem->prev = new_elem;
+	new_elem->prev = self->prev;
+	new_elem->next = self;
+	self->prev = new_elem;
 	if (NULL != new_elem->prev)
 		new_elem->prev->next = new_elem;
 }
 
-void list_insert_after(list_t *elem, list_t *new_elem)
+void list_elem_insert_after(list_elem_t *self, list_elem_t *new_elem)
 {
-	ASSERT(NULL != elem);
+	ASSERT(NULL != self);
 	ASSERT(NULL != new_elem);
 
-	new_elem->prev = elem;
-	new_elem->next = elem->next;
-	elem->next = new_elem;
+	new_elem->prev = self;
+	new_elem->next = self->next;
+	self->next = new_elem;
 	if (NULL != new_elem->next)
 		new_elem->next->prev = new_elem;
 }
 
-int elem_find(list_t *self, list_t *elem)
+void list_init(list_t *self)
 {
-	if (NULL == self)
-		return 0;
+	self->head = NULL;
+	self->tail = NULL;
+}
 
-	if (self == elem)
-		return 1;
+void list_insert_head(list_t *self, list_elem_t *elem)
+{
+	if (NULL != self->head)
+		list_elem_insert_before(self->head, elem);
 
-	return elem_find(self->next, elem);
+	self->head = elem;
+
+	if (NULL == self->tail)
+		self->tail = elem;
+}
+
+void list_insert_tail(list_t *self, list_elem_t *elem)
+{
+	if (NULL != self->tail)
+		list_elem_insert_after(self->tail, elem);
+
+	self->tail = elem;
+
+	if (NULL == self->head)
+		self->head = elem;
+}
+
+int list_find_elem(list_t *self, list_elem_t *elem)
+{
+	int ret = -1;
+	list_elem_t *check = self->head;
+
+	while (NULL != check) {
+		if (elem == check) {
+			ret = 1;
+			goto Found;
+		}
+		check = check->next;
+	}
+
+	ret = 0;
+Found:
+	return ret;
 }

44ceedd038cbb2af6242a42e7e1f530a35146f87
HsuBokai <exp2.718281@gmail.com>
2019-02-09 13:46:19 +0800

    test_thread()

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -9,6 +9,24 @@ int in_a=1, in_b=2, out_sum;
 
 static void print_thread(void *arg);
 
+static void test_thread(void)
+{
+	thread_t *thread_a = (thread_t*) malloc_page(PF_KERNEL, 1);
+	thread_init(thread_a, "thread A", 3);
+	thread_func_setup(thread_a, print_thread, "thread A! ");
+	thread_ready(thread_a);
+
+	thread_t *thread_b = (thread_t*) malloc_page(PF_KERNEL, 1);
+	thread_init(thread_b, "thread BB", 7);
+	thread_func_setup(thread_b, print_thread, "thread BB! ");
+	thread_ready(thread_b);
+
+	thread_t *thread_main = (thread_t*) malloc_page(PF_KERNEL, 1);
+	thread_init(thread_main, "thread Main", 1);
+	thread_func_setup(thread_main, print_thread, "thread Main! ");
+	thread_start(thread_main);
+}
+
 int main(void){
 	asm(" \
 		pusha;			\
@@ -34,20 +52,7 @@ int main(void){
 
 	//ASSERT(1==2);
 
-	thread_t *thread_a = (thread_t*) malloc_page(PF_KERNEL, 1);
-	thread_init(thread_a, "thread A", 3);
-	thread_func_setup(thread_a, print_thread, "thread A! ");
-	thread_ready(thread_a);
-
-	thread_t *thread_b = (thread_t*) malloc_page(PF_KERNEL, 1);
-	thread_init(thread_b, "thread BB", 7);
-	thread_func_setup(thread_b, print_thread, "thread BB! ");
-	thread_ready(thread_b);
-
-	thread_t *thread_main = (thread_t*) malloc_page(PF_KERNEL, 1);
-	thread_init(thread_main, "thread Main", 1);
-	thread_func_setup(thread_main, print_thread, "thread Main! ");
-	thread_start(thread_main);
+	test_thread();
 
 	while(1);
 	return 0;
